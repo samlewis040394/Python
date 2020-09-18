2349fb8cd5531306af158c85f14e4a54685905a2
Sam Lewis <samlewis040394@gmail.com>
2020-09-18 17:59:46 +0100

    Add files via upload

--- Wisdom_Generator/Wisdom_Generator.py
+++ Wisdom_Generator/Wisdom_Generator.py
@@ -1,1 +1,18 @@
+# Randomly selecting entries from my philosophical database
+def Wisdom_Generator(number_of_results=1):
+    import pandas as pd
+    import random
+    import numpy as np
+    pd.set_option('display.max_rows', None)
+    Brain = pd.read_pickle('Brain.pkl')
+    Brain = Brain.where(
+        Brain['Parent'].str.contains("What|How|Who|When|Why|What|Causes|Effects|Point|Problems|Risks") \
+        & ~Brain['Child'].str.contains(
+            "with/to|Six Ws|What|How|Who|When|Why|What|Causes|Effects|Point|Types|Problems|Parts|Risks")).dropna()
+    #print(Brain)
+    for x in range(number_of_results):
+        randy = random.randint(0, len(Brain))
+        print(Brain['Parent'].iloc[randy] \
+              + " : " + Brain['Child'].iloc[randy])
 
+Wisdom_Generator(10)
